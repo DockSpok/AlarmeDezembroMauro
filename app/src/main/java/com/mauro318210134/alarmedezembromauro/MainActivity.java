92d9a3be947ec9ac05b1408d605211c306d5aaa0
MAURO SIQUEIRA DAMASCENO <43550691+DockSpok@users.noreply.github.com>
2020-12-02 22:52:13 -0300

    Instanciados em MainActivity os elementos de layout

--- app/src/main/java/com/mauro318210134/alarmedezembromauro/MainActivity.java
+++ app/src/main/java/com/mauro318210134/alarmedezembromauro/MainActivity.java
@@ -3,12 +3,37 @@ package com.mauro318210134.alarmedezembromauro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    CheckBox checkBoxDom, checkBoxSeg, checkBoxTer, checkBoxQua, checkBoxQui, checkBoxSex, checkBoxSab;
+    Button btnAgendar;
+    EditText edtHora;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBoxDom = findViewById(R.id.checkBoxDom);
+        checkBoxSeg = findViewById(R.id.checkBoxSeg);
+        checkBoxTer = findViewById(R.id.checkBoxTer);
+        checkBoxQua = findViewById(R.id.checkBoxQua);
+        checkBoxQui = findViewById(R.id.checkBoxQui);
+        checkBoxSex = findViewById(R.id.checkBoxSex);
+        checkBoxSab = findViewById(R.id.checkBoxSab);
+
+        btnAgendar = findViewById(R.id.btnAgendar);
+        edtHora = findViewById(R.id.edtHora);
+
+        btnAgendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
